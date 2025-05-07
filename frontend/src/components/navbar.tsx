246bb21ddf47c439620aa0c411225c865c8797c9
Christopher Chen <christopherchen@vl965-172-31-39-121.wireless.umass.edu>
2025-05-07 17:02:12 -0400

    pulled doctor page

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -22,6 +22,11 @@ export default function Navbar() {
       label: "Profile",
       icon: <User className="w-5 h-5 mr-1" />,
     },
+    {
+      to: "/doctor",
+      label: "Doctors",
+      icon: <User className="w-5 h-5 mr-1" />,
+    },
   ];
 
   return (
