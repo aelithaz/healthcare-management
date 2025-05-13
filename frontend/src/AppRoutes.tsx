08584599c804d8fb8c61d53150a719d29ea85ed9
rmalmutairi <rmalmutairi@umass.edu>
2025-05-13 13:47:35 -0400

    register DoctorProfilePage route in AppRoutes

---
+++ frontend/src/AppRoutes.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from './loginpage';
+import PatientHome from './pages/patientpage/patienthome';
+import RecordsPage from './pages/patientpage/RecordsPage';
+import PatientLayout from './pages/patientpage/PatientLayout';
+import DoctorHome from './pages/doctorpage/doctorhome';
+import DoctorProfilePage from './pages/doctorpage/DoctorProfilePage';
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/doctor" element={<DoctorHome />} />
+      <Route path="/doctor/profile" element={<DoctorProfilePage />} /> {/* New route */}
+
+      {/* Patient Routes */}
+      <Route path="/patient" element={<PatientLayout />}>
+        <Route index element={<PatientHome />} />
+        <Route path="records" element={<RecordsPage />} />
+      </Route>
+
+      {/* Redirect to login if no route matches */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
